f2b0bb37db4210369f6faf6402daad6b744fafaa
ynonp <ynonperek@gmail.com>
2016-09-26 14:30:00 +0300

    FIXED syntax

--- day2/03-modules/src/inheritance.js
+++ day2/03-modules/src/inheritance.js
@@ -1,27 +1,26 @@
-function Rentable(title) {
+function Rentable (title) {
   this.title = title;
 }
 
-Rentable.prototype.isAvailable() {
+Rentable.prototype.isAvailable = function () {
   return this.title.charAt(0) === 'a';
 };
 
 Book.prototype = Object.create(Rentable.prototype);
 
-Book.prototype.isInteresting = function() {
+Book.prototype.isInteresting = function () {
 };
 
-function Book(title, author) {
+function Book (title, author) {
   Rentable.call(this, title);
   this.author = author;
 }
 
-
-function Video(title) {
+function Video (title) {
   this.title = title;
 }
 
-Video.prototype.isInteresting() {
+Video.prototype.isInteresting = function () {
   return this.title.charAt(0) === 'a';
 };
 
