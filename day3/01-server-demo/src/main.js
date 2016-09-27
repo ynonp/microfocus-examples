1c7dc352d7af120dd7981d15c7de23f6e9641db7
ynonp <ynonperek@gmail.com>
2016-09-27 12:21:35 +0300

    solution for interactive chat

--- day3/01-server-demo/src/main.js
+++ day3/01-server-demo/src/main.js
@@ -1,5 +1,43 @@
 // read: https://tocode.co.il/blog/2016-07-ajax
 //
+class ChatWindow {
+  constructor (el) {
+    this.form = el.querySelector('form');
+    this.panel = el.querySelector('ul');
+    this.btnRefresh = el.querySelector('#btnRefresh');
+    this.msgEl = this.form.querySelector('input');
+
+    this.form.addEventListener('submit', this.sendMessage.bind(this));
+    this.btnRefresh.addEventListener('click', this.refresh.bind(this));
+    this.template = Handlebars.compile(`
+      {{#each messages}}
+        <li>{{this}}</li>
+      {{/each}}
+    `);
+  }
+
+  sendMessage (e) {
+    e.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/messages');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({text: this.msgEl.value}));
+    this.msgEl.value = '';
+  }
+
+  refresh () {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/messages');
+    xhr.addEventListener('load', this.showMessages.bind(this));
+    xhr.send();
+  }
+
+  showMessages (ev) {
+    const data = JSON.parse(ev.target.responseText);
+    this.panel.innerHTML = this.template({ messages: data });
+  }
+}
+
 const templateString = `
   <div style="background-color:{{favoriteColor}}">
     <p><b>Title: </b>{{title}}</p>
@@ -36,3 +74,6 @@ function getDataFromServer (ev) {
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', getDataFromServer);
+
+const root = document.querySelector('#messages');
+const chat = new ChatWindow(root);
